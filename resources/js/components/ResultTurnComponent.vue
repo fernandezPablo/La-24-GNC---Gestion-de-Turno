aea42daa96b8e305d6f7ea25e5c9fdf153c50253
PABLO <fernandez.pablo44@gmail.com>
2019-11-21 11:42:05 -0300

    Change branch to finish previous issue first

---
+++ resources/js/components/ResultTurnComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="main-div">
+        <v-tabs
+            centered
+            dark
+        >
+            <v-tab v-for="tab in tabs" v-bind:key="tab">
+                {{ tab }}
+            </v-tab>
+        </v-tabs>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            tabs: ['GNC','ACEITE','VARIOS','TOTALES','A DECLARAR'],
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
